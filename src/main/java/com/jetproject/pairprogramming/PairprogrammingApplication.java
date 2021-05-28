b4b1511a07a457b0736bfbaa34682c6ae54423cc
declanwilkinson <declanwilkinson10@gmail.com>
2021-05-28 10:17:32 +0100

    dwi69 | Initial commit

---
+++ src/main/java/com/jetproject/pairprogramming/PairprogrammingApplication.java
@@ -0,0 +1,13 @@
+package com.jetproject.pairprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairprogrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairprogrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetproject/pairprogramming/PairprogrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetproject.pairprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairprogrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
